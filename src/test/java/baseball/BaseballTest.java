cc165efbdf00878bda477fa069ee0fbc8578a1e6
mhjang <mhjang@miridih.com>
2021-12-01 21:35:38 +0900

    12/01 숫자야구 리팩토링

--- src/test/java/baseball/BaseballTest.java
+++ src/test/java/baseball/BaseballTest.java
@@ -4,67 +4,126 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static baseball.BaseballTest.BallNumber.*;
 
 public class BaseballTest {
 
     @Test
-    @DisplayName("플레이어 생성자가 만들어질 때마다 3자리 랜덤값이 만들어진다.")
-    void cntNumDigit_EqualToLimit_WhenPlayerCreated() {
-        Player player = Player.create();
-        Assertions.assertThat(player.getAnswer().size()).isEqualTo(3);
+    @DisplayName("스트라이크 테스트")
+    void strike() {
+        Ball ball = new Ball(FIRST,1);
+        Ball answer = new Ball(FIRST,1);
+
+        Assertions.assertThat(ball.compareTo(answer)).isEqualTo(BallResult.STRIKE);
     }
 
-    final static class Player {
+    @Test
+    @DisplayName("볼 테스트")
+    void ball() {
+        Ball ball = new Ball(BallNumber.SECOND,1);
+        Ball answer = new Ball(FIRST, 1);
 
-        private final int DIGIT_LIMIT = 3;
-        private static Number answer = new Number();
+        Assertions.assertThat(ball.compareTo(answer)).isEqualTo(BallResult.BALL);
+    }
 
-        /**
-         * 서로 다른 3자리 랜덤값을 생성한다.
-         * */
-        final static Player create() {
-            Player player = new Player();
-            Number number = player.answer;
-            while (number.size() < player.DIGIT_LIMIT) {
-                int i = new Random().nextInt(10);
-                if (!number.contains(i)) number.add(i);
-            }
-            return player;
+    @Test
+    @DisplayName("정답을 맞춘다.")
+    void correct() {
+        Balls balls = new Balls(Arrays.asList(1,2,3));
+        Balls answers = new Balls(Arrays.asList(1,2,3));
+
+        Assertions.assertThat(answers.isCorrect(balls)).isTrue();
+    }
+
+
+    private class Ball {
+
+        private BallNumber pos;
+        private int value;
+
+        public Ball(BallNumber pos, int value) {
+            this.pos = pos;
+            this.value = value;
         }
 
-        /**
-         * number input은 무조건 3자리 수이다.
-         *
-         * */
-         int check(String input) {
-            int cnt = 0;
-            for (int i = 0; i < input.length() ; i++) {
-                if (answer.contains(input.charAt(i) - '0')) ++cnt;
-            }
-            return cnt;
-        }
+        public BallNumber getPos() { return pos; }
 
-        Number getAnswer() {
-             return answer;
+        public int getValue() { return value; }
+
+        public BallResult compareTo(Ball other) {
+            if (this.pos == other.getPos() && this.value == other.getValue()) return BallResult.STRIKE;
+            if (this.pos != other.getPos() && this.value == other.getValue()) return BallResult.BALL;
+            return BallResult.NOTHING;
         }
     }
 
-    static final class Number {
+    private class Balls {
 
-        private List<Integer> list = new ArrayList<>();
+        private final static int MAGIC_NUMBER = 3;
+        private List<Ball> list;
 
-        public boolean contains(int i) {
-            return list.contains(i);
+        public Balls(List<Integer> input) {
+            if (input.size() != MAGIC_NUMBER) throw new IllegalArgumentException("3자리 수만 입력이 가능합니다.");
+
+            list = new ArrayList<>();
+            Atomic<BallNumber> atomic = new Atomic();
+            input.stream().map((number) -> new Ball(atomic.getAndIncrement(), number)).collect(Collectors.toList());
         }
 
-        public int size() {
-            return list.size();
+        public boolean isCorrect(Balls other) {
+            for (int i = 0; i < list.size(); i++) {
+            }
+            return true;
+        }
+    }
+
+    enum BallNumber {
+        FIRST(1),
+        SECOND(2),
+        THIRD(3);
+
+        private int num;
+
+        BallNumber(int num) { this.num = num; }
+
+        public BallNumber increment() {
+            if (this == FIRST) return SECOND;
+            if (this == SECOND) return THIRD;
+            return FIRST;
+        }
+    }
+
+    private class Atomic<BallNumber> {
+        private BallNumber data;
+
+        public Atomic() {
+            data = (BallNumber) FIRST;
         }
 
-        public void add(int i) {
-            list.add(i);
+        public Atomic(BallNumber number) {
+            data = number;
         }
 
+        public BallNumber getAndIncrement() {
+            BallNumber curr = data;
+            /*data = data.increment();*/
+
+            if (data == FIRST) data = (BallNumber) SECOND;
+            if (data == SECOND) data = (BallNumber) THIRD;
+            if (data == THIRD) data = (BallNumber) FIRST;
+
+            return curr;
+        }
+    }
+
+    private enum BallResult {
+        STRIKE,
+        BALL,
+        NOTHING;
     }
 }
